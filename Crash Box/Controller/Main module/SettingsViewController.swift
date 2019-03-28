503e69acc98240b4beaf7327e71d683756ff2fb6
s4rv4d <sarvadshetty@gmail.com>
2019-03-29 01:51:30 +0530

    settings almost done

--- Crash Box/Controller/Main module/SettingsViewController.swift
+++ Crash Box/Controller/Main module/SettingsViewController.swift
@@ -11,12 +11,31 @@ import UIKit
 class SettingsViewController: UIViewController {
     
     //MARK: - IBOutlets
-    @IBOutlet weak var profilePicture: UIImageView!
+    @IBOutlet weak var profileImgView: UIImageView!
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        setupImageView()
     }
+    
+    //MARK: - Function
+    func setupImageView(){
+        profileImgView.layer.cornerRadius = profileImgView.frame.width / 2
+        profileImgView.layer.masksToBounds = false
+    }
+    
+    
+    //MARK: - IBActions
+    @IBAction func aboutUsTapped(_ sender: UIButton) {
+    }
+    
+    @IBAction func faqTapped(_ sender: UIButton) {
+    }
+    
+    @IBAction func tcTapped(_ sender: UIButton) {
+    }
+    
 }
